4c73b0640efb81fbf53d7a33140aeec3e251ef34
Erica Gilbert <erica.gilbert@yahoo.com>
2017-06-19 16:04:41 -0400

    updates

--- todo/js/exercise.js
+++ todo/js/exercise.js
@@ -1,37 +1,32 @@
     'use strict';
 
-var MyApp = {};
+    var MyApp = {};
 
-//addToList, removeFromList
+    //addToList, removeFromList
 
-MyApp.compileItem = function(item){
-  var source = $('#todo-template').html();
-  var template =  Handlebars.compile(source);
-  return template(item);
-}
-
-MyApp.addToList = function(list,item){
-  var itemObject = {toDo:item.val()};
-  var compiledItem = MyApp.compileItem(itemObject);
-  list.append(compiledItem);
-}
-
-$(document).ready(function(){
-  var $newTaskForm = $('#new_task');
-  var $taskList = $('#task_list');
-  var $itemRemoveBox = $('li.new_task.checkBox');
-  $newTaskForm.submit(function(event){
-    event.preventDefault();
-    var $newTaskInput = $('#new_task_input');
-    MyApp.addToList($taskList,$newTaskInput);
-  });
-  $('input[type="checkbox"]').on('click', function(){
-    if ( $(this).is(':checked') ) {
-    	console.log('I clicked on this');
-        $( "li.new_task" ).addClass('completed');
-    } 
-    else {
-        return;
+    MyApp.compileItem = function(item) {
+        var source = $('#todo-template').html();
+        var template = Handlebars.compile(source);
+        return template(item);
     }
-});
-});
+
+    MyApp.addToList = function(list, item) {
+        var itemObject = { toDo: item.val() };
+        var compiledItem = MyApp.compileItem(itemObject);
+        list.append(compiledItem);
+    }
+
+    $(document).ready(function() {
+        var $newTaskForm = $('#new_task');
+        var $taskList = $('#task_list');
+        $newTaskForm.submit(function(event) {
+            event.preventDefault();
+            var $newTaskInput = $('#new_task_input');
+            MyApp.addToList($taskList, $newTaskInput);
+        });
+        if (document.getElementById('new_complete_submit').checked) {
+            $("new_task").show();
+        } else {
+            $("#new_task").hide();
+        }
+    });
